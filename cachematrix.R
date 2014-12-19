362359c1799ecaa36d9a5eb1f2d4fbdd03432143
unknown <538321@BAHR9HXVW1.resource.ds.bah.com>
2014-12-19 15:04:47 -0500

    Commited Changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Creates the cache by setting and getting the matrix
+## The matrix that is passes is set and retrieved by get.
+## The function returns a list of R opbjects of get and set so that the calling function can access the methods
+## from the list.
+makeCacheMatrix <- function(x = matrix()){
+        m<- NULL
+        set <- function(y){
+                x <<- y
+                m <<- NULL
+                message(" step 1")
+                print(x)
+        }
 
-## Write a short comment describing this function
+        get <- function(){
+                return(x)
+                message("step2")
+        }
+        setIMatrix <- function(IMatrix){
+                m <<- IMatrix
+        }
+        getIMatrix <- function(){
+                return (m)
+        }
 
-makeCacheMatrix <- function(x = matrix()) {
-
+        return (list( set = set, get = get, setIMatrix = setIMatrix, getIMatrix = getIMatrix))
 }
 
+## Retrieves the matrix from cache and does Inverse.
+cacheSolve <- function(x){
+        data <- list()
+        m <- x$getIMatrix()
+        print(m)
+        if (!is.null(m)){
+                message("Cached Data Found")
+                return(m)
+        }else{
+                message("No Cached Data Found")
+                data <- x$get()
+                m <- solve(data)
+                x$setIMatrix(m)
+                return(m)
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        }
 }
